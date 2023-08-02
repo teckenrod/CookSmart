00d720acdd87c1520fcf93ad76ce0fcaf642f921
teckenrod <treyeck2@gmail.com>
2023-08-02 14:46:21 -0500

    Initial Commit

---
+++ CookSmart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookSmart
+//
+//  Created by Trey Eckenrod on 8/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
