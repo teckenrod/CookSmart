cfb47703ea5f13c6e443a1f8fff1cc33aa2b9ece
teckenrod <treyeck2@gmail.com>
2023-08-03 09:57:45 -0500

    UI updates and added some controllers

---
+++ CookSmart/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  CookSmart
+//
+//  Created by Trey Eckenrod on 8/3/23.
+//
+
+import Foundation
+
+struct K {
+    struct Segues {
+        
+    }
+    
+    struct Colors {
+        
+    }
+    
+    struct FStore {
+        
+    }
+}
---
+++ CookSmart/Controller/RecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeViewController.swift
+//  CookSmart
+//
+//  Created by Trey Eckenrod on 8/3/23.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
