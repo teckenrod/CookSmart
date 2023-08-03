37b2ae1eacb479e9443ddb0df186abde10087fa1
teckenrod <treyeck2@gmail.com>
2023-08-03 10:57:11 -0500

    more controllers

--- CookSmart/Controller/WelcomeViewController.swift
+++ CookSmart/Controller/WelcomeViewController.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
+
+
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "CookSmart"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
