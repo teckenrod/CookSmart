12e5f199a57d6507cce6ec4e9c919ca1577457a1
teckenrod <treyeck2@gmail.com>
2023-08-03 10:15:39 -0500

    constants file and basic color assets

--- CookSmart/Constants.swift
+++ CookSmart/Constants.swift
@@ -9,11 +9,20 @@ import Foundation
 
 struct K {
     struct Segues {
+        static let toAccount = "ShowAccount"
+        static let toPlan = "ShowPlan"
+        static let toCookbook = "ShowCookbook"
+        static let toList = "ShowList"
+        static let toStats = "ShowStats"
+        static let toRecipe = "ShowRecipe"
+        static let toPantry = "ShowPantry"
         
     }
     
     struct Colors {
-        
+        static let main = "MainColor"
+        static let lightAccent = "LightAccentColor"
+        static let darkAccent = "DarkAccentColor"
     }
     
     struct FStore {
