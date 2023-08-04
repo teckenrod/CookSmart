715b42fdc4ddfc5199e376fbecf35ec3bbd2ddbe
teckenrod <treyeck2@gmail.com>
2023-08-04 08:53:45 -0500

    added ingredient and recipe models

---
+++ CookSmart/Model/Ingredient.swift
@@ -0,0 +1,14 @@
+//
+//  Ingredient.swift
+//  CookSmart
+//
+//  Created by Trey Eckenrod on 8/4/23.
+//
+
+import Foundation
+
+struct Ingredient {
+    let item: String
+    let amount: Double
+    let unit: String
+}
---
+++ CookSmart/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  CookSmart
+//
+//  Created by Trey Eckenrod on 8/4/23.
+//
+
+import Foundation
+
+struct Recipe {
+    let user: String
+    let time: Double
+    let timeUnit: String
+    let ingredients: [Ingredient]
+}
