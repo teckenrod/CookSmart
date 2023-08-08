141849e23e7f02c25f3b43a5704d2cb1d5cf2edd
teckenrod <treyeck2@gmail.com>
2023-08-08 14:07:19 -0500

    pop up buttons work

--- CookSmart/Controller/AddRecipeViewController.swift
+++ CookSmart/Controller/AddRecipeViewController.swift
@@ -39,18 +39,72 @@ class AddRecipeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // set data sources
         ingredientTableView.dataSource = self
         instructionTableView.dataSource = self
         
+        // register nibs
         ingredientTableView.register(UINib(nibName: K.Misc.IngredientCellIndentifier, bundle: nil), forCellReuseIdentifier: K.Misc.IngredientCellIndentifier)
         instructionTableView.register(UINib(nibName: K.Misc.InstructionCellIdentifier, bundle: nil), forCellReuseIdentifier: K.Misc.InstructionCellIdentifier)
+        
+        setUpPopUpButtons()
+    }
+    
+    func setUpPopUpButtons() {
+        let popUpButtonClosure = { (action: UIAction) in
+            // print("Pop-up action")
+        }
+        
+        // time hrs pop up button
+        timeHrsDropDown.showsMenuAsPrimaryAction = true
+        var timeHrsOptions: [UIAction] = []
+        for i in 0...12 {
+            timeHrsOptions.append(UIAction(title: String(i), handler: popUpButtonClosure))
+        }
+        timeHrsDropDown.menu = UIMenu(children: timeHrsOptions)
+        
+        // time mins pop up button
+        timeMinsDropDown.showsMenuAsPrimaryAction = true
+        var timeMinsOptions: [UIAction] = []
+        for i in 0...11 {
+            timeMinsOptions.append(UIAction(title: String(i*5), handler: popUpButtonClosure))
+        }
+        timeMinsDropDown.menu = UIMenu(children: timeMinsOptions)
+        
+        // meal type pop up button
+        mealTypeDropDown.showsMenuAsPrimaryAction = true
+        mealTypeDropDown.menu = UIMenu(children: [
+            UIAction(title: "Breakfast", handler: popUpButtonClosure),
+            UIAction(title: "Meal", handler: popUpButtonClosure),
+            UIAction(title: "Snack", handler: popUpButtonClosure),
+            UIAction(title: "Dessert", handler: popUpButtonClosure)
+        ])
+        
+        // servings pop up button
+        servingsDropDown.showsMenuAsPrimaryAction = true
+        var servingsOptions: [UIAction] = []
+        for i in 1...12 {
+            servingsOptions.append(UIAction(title: String(i), handler: popUpButtonClosure))
+        }
+        servingsDropDown.menu = UIMenu(children: servingsOptions)
+        
+        // ingredient unit pop up button
+        ingredientUnitDropDown.showsMenuAsPrimaryAction = true
+        ingredientUnitDropDown.menu = UIMenu(children: [
+            UIAction(title: "C", handler: popUpButtonClosure),
+            UIAction(title: "Oz", handler: popUpButtonClosure),
+            UIAction(title: "Fl Oz", handler: popUpButtonClosure),
+            UIAction(title: "tsp", handler: popUpButtonClosure),
+            UIAction(title: "Tbsp", handler: popUpButtonClosure),
+            UIAction(title: "g", handler: popUpButtonClosure)
+        ])
     }
     
     @IBAction func addIngredient(_ sender: UIButton) {
         // get ingredient info
         let item = ingredientTextField.text!
-        let amount = Double(ingredientTextField.text!)!
+        let amount = Double(ingredientAmountTextField.text!)!
         let unit = (ingredientUnitDropDown.titleLabel?.text!)!
         
         // create ingredient item and append
@@ -72,6 +126,9 @@ class AddRecipeViewController: UIViewController {
         // get instruction info
         let instruction = instructionTextField.text!
         
+        // append instruction
+        instructions.append(instruction)
+        
         // reset instruction input
         instructionTextField.text = ""
         
@@ -89,26 +146,34 @@ class AddRecipeViewController: UIViewController {
 //MARK: - UITableViewDataSource
 extension AddRecipeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        var returnVal = 0
-        if tableView == ingredientTableView {
-            returnVal = ingredients.count
-        } else if  tableView == instructionTableView {
-            returnVal = instructions.count
+        switch(tableView) {
+        case ingredientTableView:
+            return ingredients.count
+        case instructionTableView:
+            return instructions.count
+        default:
+            return 0
         }
-        return returnVal
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var returnCell = UITableViewCell()
-        if tableView == ingredientTableView {
-            let returnCell = tableView.dequeueReusableCell(withIdentifier: K.Misc.IngredientCellIndentifier, for: indexPath) as! IngredientTableViewCell
-            returnCell.ingredientLabel.text = ingredients[indexPath.row].item
-            returnCell.amountLabel.text = String(ingredients[indexPath.row].amount)
-        } else if  tableView == instructionTableView {
-            let returnCell = tableView.dequeueReusableCell(withIdentifier: K.Misc.InstructionCellIdentifier, for: indexPath) as! InstructionTableViewCell
-            returnCell.instructionLabel.text = instructions[indexPath.row]
-            returnCell.instructionNumLabel.text = String(instructions.count)
+        print("Cell: \(indexPath.row)")
+        switch(tableView) {
+        case ingredientTableView:
+            print("Ingredient Table View")
+            let cell = tableView.dequeueReusableCell(withIdentifier: K.Misc.IngredientCellIndentifier, for: indexPath) as! IngredientTableViewCell
+            cell.ingredientLabel?.text = ingredients[indexPath.row].item
+            cell.amountLabel?.text = "\(ingredients[indexPath.row].amount) \(ingredients[indexPath.row].unit)"
+            return cell
+        case instructionTableView:
+            print("Instruction Table View")
+            let cell = tableView.dequeueReusableCell(withIdentifier: K.Misc.InstructionCellIdentifier, for: indexPath) as! InstructionTableViewCell
+            cell.instructionLabel?.text = instructions[indexPath.row]
+            cell.instructionNumLabel?.text = String(instructions.count)
+            return cell
+        default:
+            print("No Table View")
+            return UITableViewCell()
         }
-        return returnCell
     }
 }
